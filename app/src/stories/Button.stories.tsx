7eb155538df79a713a4ce1d8452afa24ce1c62ef
Dan Greco <atcsumboxcar182@hotmail.com>
2019-10-23 19:41:32 -0400

    Added new stories

---
+++ app/src/stories/Button.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import { Button } from '../components/Button';
+
+export default {
+    title: 'Button'
+};
+
+export const DefaultButton = () => <Button />;
