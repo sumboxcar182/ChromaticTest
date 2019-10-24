4d2fa780a08b63e796f4baf709238d2e6bb74cce
Dan Greco <atcsumboxcar182@hotmail.com>
2019-10-23 20:13:28 -0400

    Added textbox

---
+++ app/src/components/TextBox.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+export const TextBox = () => {
+    return (
+        <input>
+        </input>
+    )
+}
---
+++ app/src/stories/TextBox.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import { TextBox } from '../components/TextBox';
+
+
+export default {
+  title: 'TextBox',
+};
+
+export const DefaultTextBox = () => <TextBox />;
